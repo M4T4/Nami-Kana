b391266afee1936ba4a6fb327f4d4eaee3b7d30c
Matt Windows <matthewrc95@gmail.com>
2026-01-28 20:43:37 -0800

    added list and methoid

--- lib/nami_kana/cli.rb
+++ lib/nami_kana/cli.rb
@@ -23,8 +23,9 @@ module NamiKana
         case input
         when "1"
           puts "You chose Katakana"
+          NamiKana::Menu.katakana
         when "2"
-          puts "You chose Hiragana222"
+          puts "You chose Hiragana"
           NamiKana::Menu.hiragana
         else
           puts "Invalid option"
@@ -32,33 +33,33 @@ module NamiKana
       end
     end
 
-    desc "learn TYPE", "Learn kana: hiragana or katakana"
-    def learn(type = "hiragana")
-      puts "📚 Learning #{type.capitalize}!"
-      kana = {
-        'hiragana' => %w[あ い う え お],
-        'katakana' => %w[ア イ ウ エ オ]
-      }
-      kana[type.downcase]&.each { |k| puts " -> #{k}" } || puts("❌ Unknown type: #{type}")
-    end
+    # desc "learn TYPE", "Learn kana: hiragana or katakana"
+    # def learn(type = "hiragana")
+    #   puts "📚 Learning #{type.capitalize}!"
+    #   kana = {
+    #     'hiragana' => %w[あ い う え お],
+    #     'katakana' => %w[ア イ ウ エ オ]
+    #   }
+    #   kana[type.downcase]&.each { |k| puts " -> #{k}" } || puts("❌ Unknown type: #{type}")
+    # end
 
-    desc "quiz", "Take a kana quiz"
-    def quiz
-      pairs = {
-        'あ' => 'a', 'い' => 'i', 'う' => 'u', 'え' => 'e', 'お' => 'o'
-      }
-      score = 0
-      pairs.shuffle.to_h.each do |kana, romaji|
-        print "👉 What is '#{kana}'? "
-        answer = $stdin.gets.strip
-        if answer.downcase == romaji
-          puts "✅ Correct!"
-          score += 1
-        else
-          puts "❌ Nope. It was '#{romaji}'"
-        end
-      end
-      puts "🏁 Final Score: #{score}/#{pairs.size}"
-    end
+    # desc "quiz", "Take a kana quiz"
+    # def quiz
+    #   pairs = {
+    #     'あ' => 'a', 'い' => 'i', 'う' => 'u', 'え' => 'e', 'お' => 'o'
+    #   }
+    #   score = 0
+    #   pairs.shuffle.to_h.each do |kana, romaji|
+    #     print "👉 What is '#{kana}'? "
+    #     answer = $stdin.gets.strip
+    #     if answer.downcase == romaji
+    #       puts "✅ Correct!"
+    #       score += 1
+    #     else
+    #       puts "❌ Nope. It was '#{romaji}'"
+    #     end
+    #   end
+    #   puts "🏁 Final Score: #{score}/#{pairs.size}"
+    # end
   end
 end
--- lib/nami_kana/menu/hiragana.rb
+++ lib/nami_kana/menu/hiragana.rb
@@ -1,11 +1,69 @@
 # lib/nami_kana/menu/hiragana.rb
+require 'json'
 
 module NamiKana
   module Menu
     def self.hiragana
-      puts "🧠 Practicing Hiragana..."
-      kana = %w[あ い う え お]
-      kana.each { |char| puts " -> #{char}" }
+      puts "Select group to study"
+      input = $stdin.gets.strip
+
+      case input
+        when "1"
+          puts "Gojūon"
+          NamiKana::Menu.katakana
+        when "2"
+          puts "Youon"
+          NamiKana::Menu.hiragana
+        when "3"
+          puts "Sokuon"
+          NamiKana::Menu.hiragana
+        when "3"
+          puts "Tokushuon"
+          NamiKana::Menu.hiragana
+        else
+          puts "Invalid option"
+        end
+    end
+
+    def gojuuon
+      elements = get_elements_for_quiz(5, "hiragana", "gojuuon")
+
+      puts "Write romaji form for: "
+      puts elements.first.hiragana
+
+      input = $stdin.gets.strip
+
+      if input == elements.first.romaji
+        puts "Correct"
+      else
+        puts "Nope"
+      end
+      
+
+    end
+
+    private
+
+    def get_kana_elements_from_list(n, kana, family)
+      # Load JSON
+      file_path = File.expand_path("../../data/kana_list.json", __dir__)
+      json = JSON.parse(File.read(file_path))
+
+      items = kanalist[:family].sample(5)
+      items
+    end
+
+    def family(i)
+      case i
+      when "Gojūon"
+        gojuuon
+      when "Youon"
+        youon
+      when "Sokuon"
+        sokuon
+      when "Tokushuon"
+        tokushuon
+      end
     end
   end
 end
